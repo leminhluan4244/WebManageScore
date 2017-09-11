51121e0441faec673e45eea3c61e65cd87aca4c0
thoandroid <huynh.tho888@gmail.com>
2017-09-11 11:22:55 +0700

    update chi hoi

---
+++ controller/branch/branch.info.php
@@ -0,0 +1,33 @@
+<div id="infoBranch" class="modal fade " tabindex="-1" role="dialog" aria-labelledby aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Thông tin Chi Hội</h4>
+            </div>
+            <div class="modal-body ">
+                <form action="#" method="post">
+
+                    <fieldset class="form-group">
+                        <p class="text-left "><b>Mã Chi Hội</b></p>
+                        <p class="text-left form-control"><b>CH001</b></p>
+                    </fieldset>
+
+                    <fieldset class="form-group">
+                        <p class="text-left"><b>Tên Chi Hội</b></p>
+                        <p class="text-left form-control"><b>Phụng Hiệp</b></p>
+                    </fieldset>
+
+                    <fieldset class="form-group">
+                        <p class="text-left"><b>Tỉnh Thành</b></p>
+                        <p class="text-left form-control"><b>Hậu Giang</b></p>
+                    </fieldset>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Đóng</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
