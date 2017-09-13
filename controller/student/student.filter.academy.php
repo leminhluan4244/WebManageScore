a7f7f31985e17c5c7b60fd00548893d369bb4054
Minh Luan Le <leminhluan4244@gmail.com>
2017-09-13 13:53:32 +0700

    update
    update 13/09

--- controller/student/student.filter.academy.php
+++ controller/student/student.filter.academy.php
@@ -3,6 +3,15 @@
     <select name="academy"  class="form-control">
         <option value="Công nghệ thông tin và truyền thông">--Chọn theo khoa--
         </option>
+        <?php
+            $acdemyM = new AcademyMod();
+            $list = array();
+            $list = $acdemyM->getAcademy();
+            foreach ($list as $key=>$value)
+            echo '
+            <option value="Công nghệ thông tin và truyền thông"></option>
+            ';
+        ?>
         <option value="Công nghệ thông tin và truyền thông">Công nghệ thông tin và truyền thông
         </option>
         <option value="Thủy sản">Thủy sản</option>
