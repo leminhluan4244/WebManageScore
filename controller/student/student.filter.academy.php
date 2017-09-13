a7779af562222c1b011d31747e7a20b1d05de094
Minh Luan Le <leminhluan4244@gmail.com>
2017-09-13 14:34:23 +0700

    update
    update 13/09 2h30

--- controller/student/student.filter.academy.php
+++ controller/student/student.filter.academy.php
@@ -7,16 +7,12 @@
             $acdemyM = new AcademyMod();
             $list = array();
             $list = $acdemyM->getAcademy();
-            foreach ($list as $key=>$value)
-            echo '
-            <option value="Công nghệ thông tin và truyền thông"></option>
+            foreach ($list as $key=>$value) {
+                echo '
+                 <option value="'.$value->getIdAcademy().'">'.$value->getAcademyName().'</option>
             ';
+            }
         ?>
-        <option value="Công nghệ thông tin và truyền thông">Công nghệ thông tin và truyền thông
-        </option>
-        <option value="Thủy sản">Thủy sản</option>
-        <option value="Môi trường">Môi trường</option>
-        <option value="Công nghệ">Công nghệ</option>
     </select>
 </div>
 <!--End combobox academy for student-->
--- controller/student/student.filter.class.php
+++ controller/student/student.filter.class.php
@@ -3,11 +3,16 @@
     <select name="class" class="form-control">
         <option value="Công nghệ thông tin và truyền thông">--Chọn theo lớp--
         </option>
-        <option value="Công nghệ thông tin và truyền thông">Kỹ thuật phần mềm 1 K40
-        </option>
-        <option value="Thủy sản">Kỹ thuật phần mềm 2 K40</option>
-        <option value="Môi trường">Kỹ thuật phần mềm 3 K40</option>
-        <option value="Công nghệ">Công nghệ thông tin 1 K40̣</option>
+        <?php
+        $classM = new ClassMod();
+        $list = array();
+        $list = $classM->getClass();
+        foreach ($list as $key=>$value) {
+            echo '
+                 <option value="'.$value->getIdClass().'">'.$value->getClassName().'</option>
+            ';
+        }
+        ?>
     </select>
 </div>
 <!--End combobox class for student-->
