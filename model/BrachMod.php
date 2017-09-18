260380cfc72e15dcb1e70849a5b2bd1ede3f955e
Minh Luan Le <leminhluan4244@gmail.com>
2017-09-18 22:36:04 +0700

    update
    update

--- model/BrachMod.php
+++ model/BrachMod.php
@@ -236,7 +236,7 @@ class BranchMod
         // Thực thi câu lệnh
         // Thực hiện câu truy vấn
         $this->conn->Connect();
-        if ($this->conn->conn->query($sql1) === true && $this->conn->conn->query($sql2)) {
+        if ($this->conn->conn->query($sql1) === true && $this->conn->conn->query($sql2)===true) {
             //echo "Xóa thành công";
             //Ngắt kết nối
             $this->conn->Stop();
