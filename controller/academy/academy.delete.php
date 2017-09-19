1a7a60c7d070ebbd1c8131910e6941579e401a0a
Minh Luan Le <leminhluan4244@gmail.com>
2017-09-19 21:09:32 +0700

    update
    cập nhật khoa thêm và xóa 19/09 9h  pm

--- controller/academy/academy.delete.php
+++ controller/academy/academy.delete.php
@@ -1,3 +1,15 @@
+<?php
+if(isset($_POST['xoa'])) {
+    $academyO = new AcademyObj();
+    $academyM = new AcademyMod();
+    foreach ($_POST['xoa'] as $key=> $value){
+        $academyO->setIdAcademy($value);
+        $academyM->deleteAcademy($academyO);
+        echo'<META http-equiv="refresh" content="0;URL=academy.manage.php">';
+    }
+
+}
+?>
 <!--Start delete Academy-->
 <div id="deleteAcademy" class="modal fade " tabindex="-1" role="dialog" aria-labelledby aria-hidden="true">
     <div class="modal-dialog">
@@ -9,15 +21,18 @@
             <div class="modal-body">
                 <h4>Hành động này cần xác nhận: Không thể hoàn tác!</h4>
                 <p>Vui lòng kiểm tra cẩn thận!</p>
-                <form action="academy.manage.php" method="post">
                     <div class="modal-footer">
                         <input type="hidden" name="deleteAcademy" id="deleteAcademy">
-                        <button type="submit" name="deleteYes" class="btn btn-danger">Đồng ý</button>
+                        <button type="submit" name="deleteYes" onclick="submitform();" class="btn btn-danger">Đồng ý</button>
                         <button type="button" class="btn btn-default" data-dismiss="modal">Không</button>
                     </div>
-                </form>
             </div>
         </div>
     </div>
 </div>
+<script>
+    function submitform() {
+        $('#manageForm').submit();
+    }
+</script>
 <!--End delete Academy-->
--- controller/academy/academy.table.php
+++ controller/academy/academy.table.php
@@ -1,3 +1,4 @@
+<form action="academy.manage.php" method="post" id="manageForm">
 <table class="table table-bordered table-condensed">
 
     <thead>
@@ -5,7 +6,7 @@
         <th>STT</th>
         <th>Mã khoa - viện</th>
         <th>Tên khoa viện</th>
-        <th>Chọn</th>
+        <th>Tất cả <br /><input type="checkbox" onClick="toggle(this)"></th>
     </tr>
     </thead>
 
@@ -15,21 +16,30 @@
     <?php
     $arrayAcademy = array();
     $arrayAcademy = $academyMod->getAcademy();
+    $i=0;
     foreach ($arrayAcademy as $key=>$value){
         echo '<tr>
-        <td>1</td>
+        <td>'.++$i.'</td>
         <td>
             <a class="align-self-center " data-toggle="modal" data-target="#infoStudent" method="get" href="student.manage.php?id='.$value->getIdAcademy().'">'.$value->getIdAcademy().'</a>
         </td>
         <td>
             <a class="align-self-center " data-toggle="modal" data-target="#infoStudent" method="get" href="student.manage.php?id='.$value->getIdAcademy().'">'.$value->getAcademyName().'</a>
         </td>
-        <td>
-            <input type="checkbox" name="idAcademy" value="idAcademy">
-        </td>
+        <td><input type="checkbox" name="xoa[]" id="'.$value->getIdAcademy().'" value="'.$value->getIdAcademy().'"/> </td>
+       
     </tr>';
     }
 
     ?>
     </tbody>
 </table>
+</form>
+<script language="JavaScript">
+    function toggle(checkall) {
+        checkboxes = document.getElementsByName('xoa[]');
+        for(var i=0, n=checkboxes.length;i<n;i++)
+        {checkboxes[i].checked = checkall.checked;
+        }
+    }
+</script>
