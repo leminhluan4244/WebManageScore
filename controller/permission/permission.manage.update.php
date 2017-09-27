38f3f905ae8a5241b4ef1b75b0ffa50962fe8190
nhut-git <nhutminhdoan.com@gmail.com>
2017-09-27 20:33:24 +0700

    Cập nhật phân quyền

--- controller/permission/permission.manage.update.php
+++ controller/permission/permission.manage.update.php
@@ -3,24 +3,31 @@ require_once("../../model/ConnectToSQL.php");
 require_once("../../model/PermissionObj.php");
 require_once("../../model/PermissionMod.php");
 switch ($_POST['btn-submit']) {
-  case 'save':
-  echo('done!');
-  //var_dump($_POST['select']);
-  // if(!empty($_POST['checkbox'])){
-  //   $checkbox = $_POST['checkbox'];
-  //   if(is_array($checkbox)){
-  //     foreach ($checkbox as $key => $value) {
-  //
-  //     }
-  //   }
-  // }
-    break;
   case 'add':
     $newPermissionObj = new PermissionObj();
     $newPermissionMod = new PermissionMod();
     $newPermissionObj->setPermissionObj($_POST['txt-namePermission'], 'EMPTY');
     $newPermissionMod->addPermission($newPermissionObj);
     break;
+  default:
+   $newPermissionObj = new PermissionObj();
+   $newPermissionMod = new PermissionMod();
+   $newPermissionMod->delete($_POST['btn-submit']);
+   if(!empty($_POST['checkbox'])){
+     $checkbox = $_POST['checkbox'];
+     if(is_array($checkbox)){
+       foreach ($checkbox as $key => $value) {
+         $newPermissionObj = new PermissionObj();
+         $newPermissionMod = new PermissionMod();
+         $newPermissionObj->setPermissionObj($_POST['btn-submit'], $value);
+         $newPermissionMod->addPermission($newPermissionObj);
+       }
+     }
+   }else{
+     $newPermissionObj->setPermissionObj($_POST['btn-submit'], 'EMPTY');
+     $newPermissionMod->addPermission($newPermissionObj);
+   }
+  break;
 }
 echo '<script>window.location.assign("../../view/permission.manage.php")</script>';
 ?>
