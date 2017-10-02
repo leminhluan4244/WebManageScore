66c158aabaae23996d1bb0359de5e33235cbd10e
nhut-git <nhutminhdoan.com@gmail.com>
2017-10-02 17:01:45 +0700

    yêu cầu đăng nhập

--- view/main.php
+++ view/main.php
@@ -8,6 +8,11 @@
 ?>
 <?php require_once '../controller/header.php'; ?>
 <?php require_once '../controller/permission/function.display.php'; ?>
+<?php
+ if(empty(getSession("userToken")['permission'])){
+  echo '<script>window.location.assign("../controller/account/account.login.php")</script>';
+ }
+?>
 <div class="container main-container">
     <?php require_once '../controller/main/main.user.info.php'; ?>
     <?php require_once '../controller/main/main.panel.php'; ?>
