a7bf2e20bb6e4c89bb59cc4dee9600d9efa07faf
nhut-git <nhutminhdoan.com@gmail.com>
2017-10-02 19:43:43 +0700

    lỗi empty

--- controller/calendar/calendar.update.php
+++ controller/calendar/calendar.update.php
@@ -4,6 +4,12 @@ require_once("../../model/PermissionObj.php");
 require_once("../../model/PermissionMod.php");
 require_once("../../model/CalendarScoringMod.php");
 require_once("../../model/CalendarScoringObj.php");
+if(empty($_POST['select']) || empty($_POST['txt-date-open']) || empty($_POST['txt-date-close'])){
+  echo '<script>
+    alert("Lỗi ngày cập nhật lịch chấm");
+  </script>';
+  echo '<script>window.location.assign("../../view/calendarScoring.php")</script>';
+}
 switch ($_POST['btn-submit']) {
   case 'save':
     if ($_POST['txt-date-open'] >= $_POST['txt-date-close']){
