a940897a8b5aa6d989a5cdeb9b2a37d3afa60fff
TanPhat <ngtanphat1896@gmail.com>
2017-10-09 16:34:29 +0700

    Biểu thức chính quy

---
+++ model/check_input.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TanPhat
+ * Date: 9/10/2017
+ * Time: 4:21 PM
+ */
+
+define("PATTERN_NAME", "/^[a-zA-Z 0-9àảãáạằẳẵắặầẩẫấậèẻẽéẹềểễếệìỉĩíịòỏõóọồổỗốộờởỡớợùủũúụừửữứựỳỷỹýỵoôơđưăâê]+$/i");
+
+/**
+ * Hàm kiểm tra tên nhập vào có hợp lệ: bao gồm Khoảng trắng, Chữ Cái và Số
+ * @param $name
+ * @return boolean
+ */
+function checkValidName($name){
+	return preg_match(PATTERN_NAME, $name) == 1;
+}
+
+/**
+ * Hàm kiểm tra hợp lệ ID: bao gồm Chữ cái (ko có dấu) và Số
+ * @param $id
+ * @return boolean
+ */
+function checkValidId($id){
+	return preg_match("/^[a-zA-Z0-9]+$/", $id) == 1;
+}
+
+/**
+ * Kiểm tra là số
+ * @param $num
+ * @return boolean
+ */
+function checkValidNumber($num){
+	return preg_match("/^\d+$/", $num) == 1;
+}
