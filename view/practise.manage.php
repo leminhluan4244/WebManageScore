5084145e35801cfff02ec64ee1a318cfcec0ce0f
TanPhat <ngtanphat1896@gmai.com>
2017-10-16 12:14:36 +0700

    No cmt

--- view/practise.manage.php
+++ view/practise.manage.php
@@ -2,26 +2,12 @@
     require_once "../controller/header.php";
 ?>
 <?php
- if(getSession("userToken")['permission'] != 'Admin'){
-  echo '<script>window.location.assign("../controller/account/account.login.php")</script>';
+ if(!isLogged()){
+  redirect("../controller/account/account.login.php");
  }
 ?>
 <div class="container-fluid">
-    <!--Start content manage accademy -->
-    <div class="container main-academy-container">
-        <div class="academy-action-list text-center">
 
-            <h4>Các học kỳ</h4>
-            <!--End filter table-->
-            <!--Begin table-->
-            <?php
-                require_once "../controller/practise/practise.content.php";
-            ?>
-            <!--End table-->
-
-        </div>
-    </div>
-    <!--End content manage student-->
 </div>
 
 <?php
