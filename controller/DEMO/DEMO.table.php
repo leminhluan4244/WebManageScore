63e475882fabd783bcffedee012cc69589816496
leminhluan4244 <leminhluan4244@gmail.com>
2017-10-16 15:02:36 +0700

    update
    update

--- controller/DEMO/DEMO.table.php
+++ controller/DEMO/DEMO.table.php
@@ -1,65 +1,4 @@
 <hr />
-<?php
-
-    $scoreAddObj = new ScoresAddObj();
-    $scoreAddMod = new ScoresAddMod();
-    /**
-    * Gửi dữ liệu qua bên này
-    */
-    #Khoi tao di tuong chua
-    $power = new PermissionMod();
-    $power->getPermissionByAccount($idLogin);
-    #Kiem tra xe thuoc phan quyen nao
-    $number1=false;
-    $number2=false;
-    $number3=false;
-    foreach ($power as $key => $value){
-    if($value->getPower()=='Thêm bảng điểm cộng trừ cho khoa' && $value->getSelected()==1) {
-    $number1 =true;
-    }
-    if($value->getPower()=='Thêm bảng điểm cộng trừ cho lớp' && $value->getSelected()==1) {
-    $number2 =true;
-    }
-    if($value->getPower()=='Thêm bảng điểm cộng trừ cho sinh viên theo chi hội' && $value->getSelected()==1) {
-    $number3 =true;
-    }
-
-    }
-    #la quan ly khoa thi cho xem danh sach hoc sinh khoa
-    if($number1){
-        $tempid = $accountM->getAcademyId($idLogin);
-        $temp1 = $accountM->getAccountStudentByAcademy($tempid);
-
-    }else $temp1=false;
-    #la quan ly chi hoi cho xem danh sach chi hoi va khong co nut loc theo lop
-    if($number2){
-        $tempid = $accountM->getCLassId($idLogin);
-        $temp2 = $accountM->getAccountStudentByClass($tempid);
-
-    } else $temp2=false;
-    #la quan ly lop hoc cho xem danh sach cac lop
-    if($number3) {
-        $tempid = $accountM->getBrachId($idLogin);
-        $temp3 = $accountM->getAccountStudentByBranch($tempid);
-    } else $temp3=false;
-    $k=0;
-    $temp = array();
-    if($temp1)
-    foreach ($temp1 as $key =>$value){
-        $temp[$k]=$value;
-        $k++;
-    }
-    if($temp2)
-    foreach ($temp2 as $key =>$value){
-        $temp[$k]=$value;
-        $k++;
-    }
-    if($temp3)
-    foreach ($temp3 as $key =>$value){
-        $temp[$k]=$value;
-        $k++;
-    }
-    ?>
     <?php
     $idLogin = getLoggedAccountId();
     $scoreMod = new ScoresAddMod();
@@ -96,7 +35,7 @@
             '.$value->getScoreName().'
         </td>
         <td>
-            '.$value->setScores().'
+            '.$value->getScores().'
         </td>
         <td>
             <a href="?idScore='.$value->getIdScore().'" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-pencil"></span></a>
