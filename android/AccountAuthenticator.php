6f0840c980e75dd12eaea8c9fc4ca531bc5b2457
IrisPham <phamhoaianctu@gmail.com>
2017-10-16 14:31:44 +0700

    Cập nhật xác thực thông tin Account

--- android/AccountAuthenticator.php
+++ android/AccountAuthenticator.php
@@ -2,19 +2,25 @@
 	require_once "../model/AccountMod.php";
 	require_once "../model/AccountObj.php";
 	require_once "../model/ConnectToSQL.php";
-	
+
+	//REQUIRE_ONCE HEADER
+	header("Content-type: application/json; charset=UTF-8");
+
 	//CREATE Account Object and Account Model
 	$accountObj = new AccountObj();
 	$accountMod = new AccountMod();
-
+	$response = array();
 	//CREATE POST
-	$idAccount = $_POST['idAccount'];
-    $password = $_POST['password'];
-
-    //Account Authentication
-    if($accountMod->checkLogin($idAccount,$password)){
-	 	echo "true";
-	}else{
-		echo "false";
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		$idAccount = $_POST['idAccount'];
+    	$password = $_POST['password'];
+    	//Account Authentication
+    	if($accountMod->checkLogin($idAccount,$password)){
+    		$response["message"] = 1;
+		}else{
+			$response["message"] = 0;
+		}
 	}
+	//ECHOING JSION response
+	echo json_encode($response);
 ?>
