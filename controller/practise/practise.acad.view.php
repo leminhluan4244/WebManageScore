1ed7f2c51ab9dc8e4e0d8d4977d2abd5f2cee162
TanPhat <ngtanphat1896@gmai.com>
2017-10-16 16:01:20 +0700

    Cập nhật xem điểm qua các học kỳ

---
+++ controller/practise/practise.acad.view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TanPhat
+ * Date: 16/10/2017
+ * Time: 3:20 PM
+ */
---
+++ controller/practise/practise.adviser.view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TanPhat
+ * Date: 16/10/2017
+ * Time: 3:19 PM
+ */
